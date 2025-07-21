67f2c0b5b54b62aa4b6bfe698268ef6cd49a0f10
Guilherme Marques <guilherme_mds@yahoo.com>
2025-07-21 12:24:35 -0300

    connect do the database

--- src/app/cadastrar-se/components/Form.tsx
+++ src/app/cadastrar-se/components/Form.tsx
@@ -1,28 +1,21 @@
+import Input from './Input';
+
 export default function Form() {
   return (
     <form className="flex flex-col gap-4">
-      <div className="flex flex-col gap-2">
-        <label htmlFor="name" className="text-sm font-semibold">
-          Nome
-        </label>
-        <input
-          type="text"
-          id="name"
-          placeholder="Digite seu nome"
-          className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="email" className="text-sm font-semibold">
-          E-mail
-        </label>
-        <input
-          type="email"
-          id="email"
-          placeholder="Digite seu e-mail"
-          className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
+      <Input id="name" type="text" label="Nome" placeholder="Digite seu nome" />
+      <Input
+        id="email"
+        type="email"
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Senha"
+        placeholder="Digite uma senha"
+      />
       <button
         type="submit"
         className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors duration-200"
---
+++ src/app/cadastrar-se/components/Input.tsx
@@ -0,0 +1,19 @@
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  id: string;
+}
+
+export default function Input({ label, id, ...rest }: InputProps) {
+  return (
+    <div className="flex flex-col gap-2">
+      <label htmlFor={id} className="text-sm font-semibold">
+        {label}
+      </label>
+      <input
+        id={id}
+        {...rest}
+        className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+    </div>
+  );
+}
